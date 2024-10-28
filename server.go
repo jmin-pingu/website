355457b45e2aa579c37d34411b886e8b79a5fdf0
jmin111301 <jonathanheromin@gmail.com>
2024-10-27 23:28:35 -0400

    updated server to initialize dbs as necessary

--- server.go
+++ server.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"mywebsite/db"
 	"mywebsite/handlers"
+	"os"
 
 	//"github.com/kaleocheng/goldmark"
 
@@ -10,6 +13,17 @@ import (
 
 func main() {
 	e := echo.New()
+
+	// Initialize db
+	dbpool, err := db.GetConnection("websitedb")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to connect to websitedb: %v\n", err)
+		os.Exit(1)
+	}
+
+	db.InitPosts(dbpool)
+
+	dbpool.Close() // Make sure to finish the transaction
 
 	// Setup routes
 	handlers.SetupRoutes(e)
