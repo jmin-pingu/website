b4c2be6b3272538bfefd6d6bff0e2f76a9e39842
jmin111301 <jonathanheromin@gmail.com>
2024-10-30 22:50:12 -0400

    adjusted upload CLI

--- upload.go
+++ upload.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"mywebsite/db"
 	"mywebsite/ds"
@@ -14,6 +16,7 @@ import (
 	"time"
 
 	"github.com/a-h/templ"
+	"github.com/jackc/pgx/v5/pgxpool"
 	mathjax "github.com/litao91/goldmark-mathjax"
 	"github.com/yuin/goldmark"
 	meta "github.com/yuin/goldmark-meta"
@@ -21,11 +24,29 @@ import (
 	"github.com/yuin/goldmark/parser"
 )
 
+// NOTE: need to consider whether I want to include the .md format in the db
+// Additionally, we need to consider whether we want method to remove entries
+
 func main() {
-	paths := os.Args[1:]
-	// Sanity check
+	VALID_DB := []string{"books", "posts"}
+
+	target := os.Args[1]
+	paths := os.Args[2:]
+
+	// Sanity checks
 	if len(paths) == 0 {
-		panic("Did not provide paths to upload (.md files)")
+		panic("Did not provide paths to upload")
+	}
+
+	found := false
+	for _, v := range VALID_DB {
+		if target == v {
+			found = true
+			break
+		}
+	}
+	if !found {
+		panic(fmt.Sprintf("Did not provide a valid db. Choose from one of the following %s", VALID_DB))
 	}
 
 	// Initialize db
@@ -37,6 +58,135 @@ func main() {
 		os.Exit(1)
 	}
 
+	switch target {
+	case "books":
+		UploadToBooks(paths)
+	case "posts":
+		UploadToPosts(dbpool, paths)
+	}
+	fmt.Println("success 🎉")
+}
+
+type Books struct {
+	Books []Book `json:"books"`
+}
+
+type Book struct {
+	Tags          []string `json:"tags"`
+	Author        []string `json:"author"`
+	Title         string   `json:"title"`
+	URL           string   `json:"url"`
+	InProgress    bool     `json:"in_progress"`
+	Completed     bool     `json:"completed"`
+	Rating        int      `json:"rating"`
+	DatePublished string   `json:"date_published"`
+	DateCompleted string   `json:"date_completed"`
+}
+
+func UploadToBooks(paths []string) {
+	// NOTE: check for appropriate thing to upload
+	fmt.Println("--------- Uploading Paths to `books` table ---------")
+	fmt.Println("Paths: %v", paths)
+
+	if len(paths) != 1 {
+		fmt.Fprintf(os.Stderr, "`upload.go` failed: there should be only one file for books table\n")
+		os.Exit(1)
+	}
+
+	path := paths[0]
+
+	if _, err := os.Stat(path); err == nil {
+		fmt.Println("valid path:", path)
+	} else if match, _ := regexp.MatchString(".json$", path); !match {
+		fmt.Fprintf(os.Stderr, "`upload.go` failed to read arguments : file should be .json\n")
+		os.Exit(1)
+	} else {
+		fmt.Fprintf(os.Stderr, "`upload.go` failed to read sql template: %v\n", err)
+		os.Exit(1)
+	}
+
+	books := ParseBooksJson(path)
+	for _, book := range books {
+		ParseBook(book)
+
+		// Based on contents of md file, upload or insert
+		if db.Exists(dbpool, "posts", "link", link) {
+			db.UploadBook(dbpool, "update", tags, title, link, date, content)
+		} else {
+			db.UploadBook(dbpool, "insert", tags, title, link, date, content)
+		}
+	}
+}
+
+func ParseBook(book Book) {
+	var (
+		date_published time.Time
+		date_completed time.Time
+		err            error
+	)
+	// Parse Books struct
+
+	// Parse date
+	date_published, err = time.Parse("2006-01-02", book.DatePublished)
+	if err != nil {
+		log.Fatalf("failed to parse date_published from json: %v", err)
+	}
+
+	if book.Completed {
+		// Parse date
+		date_completed, err = time.Parse("2006-01-02", book.DateCompleted)
+		if err != nil {
+			log.Fatalf("failed to parse date_completed from json: %v", err)
+		}
+	}
+
+	// Parse tags
+	tags_set := make(ds.Set[string], 0)
+	parsed_tags := book.Tags
+	for _, tag := range parsed_tags {
+		tags_set.Add(tag) // Per post tag set: ds.Set
+	}
+	tags := make([]string, 0)
+	for k, _ := range tags_set {
+		tags = append(tags, k)
+	}
+
+	// Parse tags
+	authors_set := make(ds.Set[string], 0)
+	parsed_authors := book.Author
+	for _, author := range parsed_authors {
+		authors_set.Add(author) // Per post tag set: ds.Set
+	}
+	authors := make([]string, 0)
+	for k, _ := range authors_set {
+		authors = append(authors, k)
+	}
+	fmt.Println("", date_published, date_completed, tags, authors)
+}
+
+func ParseBooksJson(path string) []Book {
+	// open json
+	jsonf, err := os.Open(path)
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		log.Fatalf("failed to read json file: %v", err)
+	}
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonf.Close()
+
+	// read json as a byte array.
+	bytes, _ := ioutil.ReadAll(jsonf)
+
+	// we initialize our Users array
+	var books Books
+
+	// we unmarshal bytes, the byte array
+	json.Unmarshal(bytes, &books)
+	return books.Books
+}
+
+func UploadToPosts(dbpool *pgxpool.Pool, paths []string) {
+	// NOTE: check for appropriate thing to upload
 	fmt.Println("--------- Uploading Paths to `posts` table ---------")
 	fmt.Println("Paths: %v", paths)
 	// Parse through paths
@@ -46,9 +196,6 @@ func main() {
 			fmt.Println("valid path:", path)
 		} else if match, _ := regexp.MatchString(".md$", path); !match {
 			fmt.Fprintf(os.Stderr, "`upload.go` failed to read arguments : file should be .md\n")
-			os.Exit(1)
-		} else if os.IsNotExist(err) {
-			fmt.Fprintf(os.Stderr, "`upload.go` failed to read sql template: %v\n", err)
 			os.Exit(1)
 		} else {
 			fmt.Fprintf(os.Stderr, "`upload.go` failed to read sql template: %v\n", err)
@@ -70,7 +217,7 @@ func main() {
 			db.UploadPost(dbpool, "insert", tags, title, link, date, content)
 		}
 	}
-	fmt.Println("success 🎉")
+
 }
 
 func parse_md_metadata(md []byte) ([]string, string, string, time.Time) {
