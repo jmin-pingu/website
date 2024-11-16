5c04cd8fe906d883147967c03e9826ea177202fb
jmin111301 <jonathanheromin@gmail.com>
2024-11-16 17:44:40 -0500

    updated tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,23 @@
 module.exports = {
 	content: ["pub/**/*.{templ,go}"],
 	theme: {
-		fontFamily: {
-			garamond: ['"adobe-garamond-pro"', 'serif'],
-			mono: ['monospace'],
-		},
 		extend: {
+			cursor: {
+				'creative-default': 'url(/assets/cursors/normal_select_1.cur), default',
+				'creative-text': 'url(/assets/cursors/text_select_1.cur), default',
+				'creative-pointer': 'url(/assets/cursors/link_select.cur), default',
+        		'custom': 'url(https://play.tailwindcss.com/favicons/favicon-16x16.png?v=3), default',
+			},
+			colors: {
+				gray: {
+					350: "#b8bdc6",
+				}
+			},
+			fontFamily: {
+				tnr: ['"Times New Roman"', 'serif'],
+				garamond: ['"adobe-garamond-pro"', 'serif'],
+				mono: ['monospace'],
+			},
 			spacing: {
 				'128': '32rem',
 				'144': '36rem',
