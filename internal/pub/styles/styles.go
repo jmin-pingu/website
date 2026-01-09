f625c0af7a7e55f099da191ee147066f2f7dbb90
jmin-pingu <jonathanheromin@gmail.com>
2026-01-09 01:28:50 -0600

    add styles

---
+++ internal/pub/styles/styles.go
@@ -0,0 +1,7 @@
+package styles
+
+const PageWrapper = "max-w-screen-lg mx-auto sm:px-6 lg:px-12"
+const ProseArticle = "prose max-w-none md:text-xl lg:text-lg tracking-tight"
+const HeaderStyle = "mt-4 mb-8"
+const Nav = "flex items-center lg:text-xl gap-4"
+const PageSelected = "underline font-bold"
